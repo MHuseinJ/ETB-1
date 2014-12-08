94711b923039759d2f3f8d85ce6e0ef93e8d6bd2
Edmund Ophie <edmundophie@Edmunds-MacBook-Pro.local>
2014-12-09 01:26:25 +0700

    Remove search.html page and apply search function to all page

--- search.php
+++
@@ -1,8 +0,0 @@
-<?php
-$q = trim(strip_tags($_GET['term'])); // variabel $q untuk mengambil inputan user
-$daftar = file("data.txt");
-foreach ($daftar as $data){
-	$result[] = htmlentities(stripslashes($data));
-}
-echo json_encode($result); // menampilkan data dengan format json
-?>
