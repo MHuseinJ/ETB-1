c9c29035722eef88307bb3f11a3d56945716ef6b
Edmund Ophie <edmundophie@Edmunds-MacBook-Pro.local>
2014-12-10 06:40:43 +0700

    asd

--- js/search.js
+++ js/search.js
@@ -1,5 +1,6 @@
  $(function() {
 	var availableTags = [
+						"Fundamentals of Electric Circuits",
 						"DRE",
 						"Dasar Rangkaian Elektrik",
 						"Elektrik"
@@ -11,7 +12,7 @@
 });
 
 function hasilCari(){
-	$(".main").html('<table class="table table-condensed"><thead><tr onClick="hasilCari();"><th>Nomor</th><th>Judul</th><th>Penerbit</th><th>Penjual</th><th>Harga</th><th>unduh</th></tr></thead><tbody><tr onClick="location.href = \'http://www.itb.ac.id\'"><td>1</td><td>John</td><td>Carter</td><td>DRE 2</td><td>40000</td><td><a href="bookdre.html">unduh</a></td></tr><tr><td>2</td><td>Peter</td><td>Parker</td><td>DRE</td><td>40000</td><td><a href="bookdre.html">unduh</a></td></tr><tr><td>3</td><td>John</td><td>Rambo</td><td>DRE 3</td><td>40000</td><td><a href="bookdre.html">unduh</a></td></tr></tbody></table>');
+	$(".main").html(' <div class="panel panel-default book2"><div class="panel-body"><div class="col-xs-3"><a href="bookfisika.html" class="thumbnail"><img src="img/dre.png" alt="..."></a></div><div class="col-xs-9"><a href="bookfisika.html"><h3><b>Fundamentals of Electric Circuits</b></h3></a><p class="lead"><small>Pengarang : Alexander Sadiku</small></p><p class="lead"><small>Penerbit : Wesley International International Edition</small></p><p class="lead"><small>Jumlah halaman : 649 halaman</small></p><div class="harga"><h3 class="text-success">Rp 74,999</h3><a href="shoppingcart.html"><h4 class="text-primary"><span class="glyphicon glyphicon-shopping-cart"></span><b> Add to cart</b></h4></a></div></div></div></div><!-- End of Panel -->');
 }
 	
 
